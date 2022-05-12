8a99b1cb870baaaa17d95e9c6d7c1413a8b177bb
tlatimer <tom@tomlat.com>
2022-05-12 12:48:00 -0500

    fixed some canteen stuff, other improvements

--- wanderer/Rooms.py
+++ wanderer/Rooms.py
@@ -1,4 +1,5 @@
 import random
+from time import sleep
 
 import pandas as pd
 
@@ -11,7 +12,6 @@ class RoomsTable:
         self.gs = game_state
 
         rooms_file = find_file('wanderer - rooms')
-        # items_file = find_table_file('wanderer - items')
 
         self.df = pd.read_csv(rooms_file).fillna('')
         self.rooms = list(self.df['name'])
@@ -35,20 +35,18 @@ class RoomTemplate:
         self.gs = rooms_table.gs  # game_state
         self.it = ItemsTable()
         self.loot = []
+        self.inspected = False
+        self.entry_text = self.data['entry_text'][:]
 
     def enter(self, has_choice=True):
         print(f"\n-={self.data['name']}=-")
-        print(f"\n{self.data['entry_text']}")
+        print(f"\n{self.entry_text}")
 
         if self.loot:
             self.show_loot()
 
         if has_choice:
             self.handle_choice()
-
-    def show_loot(self):
-        for item in self.loot:
-            print(f'You see an [{item}]!')
 
     def get_choice(self):
         options = ['5', '4', '3', '2', '1']
@@ -78,9 +76,9 @@ class RoomTemplate:
 
         if choice == 'Take Loot':
             self.gs.inventory += self.loot
-        elif choice == 'Inspect+':
-            print('You spend some extra time looking around.')
+        elif choice == 'Search+':
             self.inspect()
+            self.handle_choice()
         elif choice in ['Leave', 'Turn Around']:
             pass
         else:
@@ -88,12 +86,24 @@ class RoomTemplate:
         return None
 
     def inspect(self):
-        pass  # TODO: overwrite me
+        if self.inspected:
+            print("You've already searched here")
+            return
+
+        print('You spend some extra time searching the area.')
+        self.inspected = True
+        self.generate_loot(self.data['max_loot'])
 
     def generate_loot(self, max_items):
         for _ in range(random.randint(2, max_items)):
             item = self.it.get_rand_item()
             self.loot.append(item)
+
+    def show_loot(self):
+        sleep(.5)
+        for item in self.loot:
+            print(f'You found a [{item}]!')
+            sleep(.5)
 
 
 class room(RoomTemplate):
@@ -115,6 +125,7 @@ class key_loot(loot):
     def __init__(self, rooms_table, room_name):
         super().__init__(rooms_table, room_name)
         self.color = random.choice(['Red', 'Green', 'Blue'])
+        self.entry_text = self.entry_text.replace('???', self.color)
 
     def handle_choice(self):
         choice = super().handle_choice()  # returns None if it was handled generically
@@ -138,29 +149,30 @@ class trap(RoomTemplate):
         self.num_traps = random.randint(2, 10)
 
     def enter(self):
-        print(f"\n-={self.data['name']}=-")
-        print(f"\n{self.data['entry_text']}")
+        super().enter(False)
 
-        if self.loot:
-            for item in self.loot:
-                print(f'You see an [{item}]!')
-
+        sleep(.5)
         for _ in range(self.num_traps):
             self.fire_trap()
+            sleep(.5)
 
     def fire_trap(self):
-        canteen_count = self.gs.inventory.count('canteen')
-        arrow_hit = random.choice(['canteen'] * canteen_count + ['body'] * 3 + ['miss'] * 5)
+        canteen_count = self.gs.inventory.count('Canteen')
+        arrow_hit = random.choice(['canteen'] * canteen_count
+                                  + ['body'] * 2
+                                  + ['miss'] * 5)
 
         if arrow_hit == 'body':
             self.gs.hp -= 1
             print(f"An arrow hits your body! You have {self.gs.hp} hp left.")
         elif arrow_hit == 'canteen':
             print('An arrow struck your backpack and put a hole in a canteen!')
-            self.gs.inventory.remove('canteen')
-            self.gs.inventory.append('leaky canteen')
+            self.gs.inventory.remove('Canteen')
+            self.gs.inventory.append('Leaky Canteen')
         else:
             print("An arrow missed.")
+
+        self.num_traps = random.randint(2, 10)
 
 
 class eat(RoomTemplate):
@@ -190,14 +202,14 @@ class drink(RoomTemplate):
             return
         elif choice == 'Drink':
             self.gs.water = max(10, self.gs.water)
-            self.handle_choice()
-        elif choice == 'Fill Canteens':
-            num_canteens = self.gs.inventory.count('canteen') + 0.5 * self.gs.inventory.count('leaky canteen')
+            super().handle_choice()
+        elif choice == 'Fill Canteens+':
+            num_canteens = self.gs.inventory.count('Canteen') + 0.5 * self.gs.inventory.count('Leaky Canteen')
             self.gs.water = int(num_canteens) * 10
             self.gs.hp -= 1
 
 
-class sleep(RoomTemplate):
+class heal(RoomTemplate):
     def __init__(self, rooms_table, room_name):
         super().__init__(rooms_table, room_name)
 
@@ -214,6 +226,6 @@ ROOM_TYPES = {  # has to be at the bottom or else the classes aren't declared ye
     'trap': trap,
     'eat': eat,
     'drink': drink,
-    'sleep': sleep,
+    'heal': heal,
     'riddle': riddle,
 }
