39236cb69b4b6d8826a714fa81ac9f32c0c468d7
tlatimer <tom@tomlat.com>
2022-05-28 18:32:26 -0500

    I betcha it's stuck in a loop where two parents point at each other.

--- emailGroups/process_data.py
+++ emailGroups/process_data.py
@@ -4,13 +4,17 @@ import load_data_json
 def main():
     groups_dict = load_data_json.main()
 
-    # convert to Groups
+    # convert to Group objects
     groups = {}
     for name, emails in groups_dict.items():
         groups[name] = Group(name, emails)
 
     for g in groups.values():
-        g.find_parent(groups)
+        g.calc_family(groups)
+
+    group_sizes = {}
+    for name, g in groups.items():
+        group_sizes[name] = g.cum_len()
 
     raise hell
 
@@ -23,17 +27,21 @@ class Group:
         self.parent_score = 0
         self.children = []
 
-    def find_parent(self, other_groups):
+    def calc_family(self, other_groups):
         for name, g in other_groups.items():
             if name == self.name:
                 continue  # don't want to compare to yourself
 
-            score = len(g.members & self.members) / len(g.members | self.members)
+            score = len(g.members & self.members) / len(g.members | self.members)  # maybe do log() of denominator?
             if score > self.parent_score:
                 self.parent = name
                 self.parent_score = score
 
-        # other_groups[self.parent].children.append(self.name)
+        if self.parent:
+            other_groups[self.parent].children.append(other_groups[self.name])
+
+    def cum_len(self):
+        return len(self.members) + sum([i.cum_len() for i in self.children])
 
 
 if __name__ == '__main__':
